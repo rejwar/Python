8ad5da84889a4d627c3f509dc9de07326c706877
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-05 12:36:29 +0600

    Create list.py

---
+++ list.py
@@ -0,0 +1,12 @@
+
+list =['abcd',786,2.23, 'john', 70.2]
+tinylist =[ 123, 'john']
+
+print(list)           # Output: ['abcd', 786, 2.23, john, 70.2]
+print(tinylist)       # Output: ['abcd
+
+print(list[0]) # Output:
+print(list[1:3]) # Output: [786, 2.23]
+print(list[2:]) # Output: [2.23, 'john', 70.2]
+print(tinylist * 2) # Output: ['abcd', 'john', 123, 'john']
+print(list + tinylist) # Output: ['abcd', 786, 2.23, 'john', 70.2, 'abcd', 'john']
