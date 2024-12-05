e589cc3b299d848bda062e9830a47db96535e241
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-05 11:10:54 +0600

    Create variables2.py

---
+++ variables2.py
@@ -0,0 +1,8 @@
+a =100
+print("Tha type of variable having value" ,a ,type(a))
+
+b = 1.04
+print("The type of variable having value" ,b, type(b))
+
+c= 10+3j
+print("The type of variable having value" ,c, type(c))
