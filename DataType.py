902695553c495647f036f9d56d1cb53fe4dab320
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-05 15:31:59 +0600

    Create DataType.py

---
+++ DataType.py
@@ -0,0 +1,28 @@
+a =1 
+b =4 
+
+print(bool(a==b))
+print(a==b)
+
+a = None
+print(bool(a))
+
+a = ()
+print(bool(a))
+
+a = 0.0
+print(bool(a))
+
+a =10
+print(bool(a))
+
+print(type(234))
+print(type(9.99))
+
+a =10
+b =10.2
+c = 'hello world'
+
+print(type(a))
+print(type(b))
+print(type(c))
