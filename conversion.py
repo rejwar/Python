d375a0370ff837d18215a0fba50551a17676fc77
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-05 15:59:35 +0600

    Create conversion.py

---
+++ conversion.py
@@ -0,0 +1,28 @@
+print("Conversion to integer data format")
+
+a = int(1)
+b = int(2.2)
+c = int("3.3")
+print (a)
+print (b)
+print (c)
+
+print("Conversion to floating point number")
+
+
+a = float(1)
+b = float(2.2)
+c = float("3.3")
+print(a)
+print(b)
+print(c)
+
+
+print("Conversion to string")
+
+a  = str(1)
+b = str(2.2)
+c = str("3.3")
+print(a)
+print(b)
+print(c)
