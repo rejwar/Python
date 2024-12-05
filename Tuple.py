476b5ded7513f97ec195abfd6fe05a754111cf6e
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-05 14:27:29 +0600

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,14 @@
+tuple = ('abc', 786, 2.23, 'John', 70.2)
+tinyTuple= (123, 'John')
+
+print(tuple)
+print(tinyTuple)
+
+print(tuple[1:3])
+print(tuple[2:])
+print(tinyTuple[3:])
+print(tinyTuple*2 )
+print(tinyTuple+tuple)
+
+for i in range(6):
+ print(i)
