4995f2b6227b689f3e8bec0c446719d1bba6fc59
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-06 16:37:06 +0600

    Create NumberComparison.py

---
+++ NumberComparison.py
@@ -0,0 +1,9 @@
+print("Comparison of int and float values")
+a =10
+b = 10.0
+
+print("a =",a,"b=",b, "a>b is ", a>b)
+print("a =",a,"b=",b, "a<b is ", a<b)
+print("a =",a,"b=",b, "a==b is ", a==b)
+print("a =",a,"b=",b, "a!=b is ", a!=b)
+print("a =",a,"b=",b, "a>=b is ", a>=b)
