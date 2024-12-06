3e4c0d5e59576e83e204e4961177d400e5a16eb0
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-06 17:02:19 +0600

    Create AugmentedSubstraction.py

---
+++ AugmentedSubstraction.py
@@ -0,0 +1,19 @@
+a =10
+b = 5 
+print("Augmented substraction of int and int ")
+a -= b
+print("a= ",a,"type(a):",type(a))
+
+
+
+a=10
+b =5.5
+print("Augmented substraction of int and float")
+a-=b
+print("a= ",a,"type(a):",type(a))
+
+a=10.50
+b =5+6j
+print("Augmented substraction of float and complex")
+a-=b
+print("a= ",a,"type(a):",type(a))
