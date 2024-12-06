e37e15c8e34a21e57a83e147f1243e39f07ad14c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-06 16:43:22 +0600

    Create ComparisonOfBoolean.py

---
+++ ComparisonOfBoolean.py
@@ -0,0 +1,8 @@
+print("comparison of booleans")
+a = True
+b = False
+
+print("a=",a, "b=",b, "a<b is ",a<b)
+print("a=",a, "b=",b, "a>b is ",a>b)
+print("a=",a, "b=",b, "a==b is ",a==b)
+print("a=",a, "b=",b, "a!=b is ",a!=b)
