481e4047ee70de7751a7f348e302504ecbc78caf
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-06 16:50:03 +0600

    Create CompareTuple.py

---
+++ CompareTuple.py
@@ -0,0 +1,13 @@
+#print("Comparison of different sequenc types")
+##a = (1, 2, 3)
+#b = [1, 2, 3]
+#print("a",a,"b=",b ,"a<b is ",a<b)
+
+
+print("Comparison of tuples")
+a= (1, 2, 3)
+b = (1, 2, 3)
+print("a",a,"b=",b,"a<b is ",a<b)
+print("b",b,"b=",b,"b<b is ",a>b)
+print("a",a,"b=",b,"a<b is ",a==b)
+print("b",b,"b=",b,"b<b is ",a!=b)
