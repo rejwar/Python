68d0918063938000ebd8b64b4f4ea8b3e01b9b8c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-06 09:08:46 +0600

    Create Dictionary Type Literal.py

---
+++ Dictionary Type Literal.py
@@ -0,0 +1,14 @@
+L1 = [1,"ravi",75.5,True]
+print(L1,type(L1))
+
+T1 = [1,"ravi",75,True]
+print(T1,type(T1))
+
+capitals = {"usa": "New York", "franc" :"paris", "japan" :"tokyo"}
+numbers = {1: "one ", 2: "two", 3: "three", 4: "four"}
+points ={"p1" : (10,10) , "p2" : (20,20) }
+
+
+print(capitals,type(capitals))
+print(numbers,type(numbers))
+print(points,type(points))
