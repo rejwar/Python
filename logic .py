a09ee86eb9d49bc054a981449f16deafef666279
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-06 16:34:31 +0600

    Create logic .py

---
+++ logic .py
@@ -0,0 +1,10 @@
+print("Both operands are integer")
+
+a =5 
+b =7
+
+print("a=",a,"b=",b , "a>b is ",a>b)
+print("a=",a,"b=",b , "a<b is ",a<b)
+print("a=",a,"b=",b , "a==b is ",a==b)
+print("a=",a,"b=",b , "a!=b is ",a!=b)
+print("a=",a,"b=",b , "a>=b is ",a>=b)
