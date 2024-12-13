3cd87d4086a403fbe63f108e8dc9b905f7a882c8
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-13 23:17:47 +0600

    Create Total.py
    Signed-off-by: Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>

---
+++ Total.py
@@ -0,0 +1,11 @@
+numbers = (34,54,67,68,69,70,91,92,93,94,95)
+total =0 
+for num in numbers:
+    total += num
+print("Total = ", total)
+
+numbers = (34,54,67,68,69,70,91,92)
+total = 0 
+for num in numbers:
+    total += num 
+    print("Total = ", total)
