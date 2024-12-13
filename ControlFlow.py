f8ac4f8ad2fee13246bf30f703db72035b628bfa
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-13 10:57:52 +0600

    Create ControlFlow.py
    Signed-off-by: Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>

---
+++ ControlFlow.py
@@ -0,0 +1,60 @@
+'''worda = ["one", "two","three"]
+for x in worda:
+    print(x)
+    
+    
+i =1 
+while i < 6 :
+    print(i)
+    
+    i+=1
+    
+    
+x =0
+while x <10:
+    print("x:",x)
+    if x ==5:
+        print("Breaking")
+        break
+    x +=1
+    
+print("End")
+
+
+
+
+x =0
+while x <10:
+    print("x:",x)
+    if x ==5:
+     print("Breaking")
+    break
+    x +=1
+print("End")
+
+
+
+x = 0
+while x <10:
+    print("x:",x)
+    if x ==5:
+        print("Breaking")
+        break 
+    x+=1
+print("End")
+
+
+for letter in "python":
+    if letter =="h":
+        continue
+    print('Current letter :',letter)
+'''
+
+for letter in "python":
+    if letter =="h":
+        continue
+    print('Current letter   :',letter)
+    
+    
+    
+
