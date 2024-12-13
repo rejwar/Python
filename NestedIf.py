ec2f5f34cc7417466385c2be43ceccc7b820138e
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-13 12:11:11 +0600

    Create NestedIf.py
    Signed-off-by: Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>

---
+++ NestedIf.py
@@ -0,0 +1,38 @@
+'''num = 36 
+print ("num =",num)
+if num % 2 ==0:
+    if num % 3 == 0:
+        print ("Division by 3 and 2 ")
+print("executions ends...")
+
+num =36 
+print("num = ",num )
+if num %2 ==0:
+    if num %3 == 0:
+        print("Division by 3 and 2 ")
+print("executions ends...")
+
+num =72
+print("num = ",num)
+if num %3 ==0:
+    if num %2 == 0:
+        print("Division by 3 and 2 ")
+print("executions ends...")
+'''
+
+
+
+num =8 
+print ("num = ", num )
+if num %2 == 0:
+    if num %3 == 0:
+        print ("Divisible by 2 and 3 ")
+    else:
+        print("Not divisible by 2 and 3 ")
+        
+else:
+    if num %3==0:
+        print("Divisible by 3 not divisible 2")
+    else:
+        print("Divisible by 2")
+        
