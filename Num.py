a37452f2f787a85df94c67ebe0da855866a8bd15
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-13 23:49:49 +0600

    Create Num.py
    Signed-off-by: Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>

---
+++ Num.py
@@ -0,0 +1,21 @@
+'''for num in range(5):
+    print(num,end= '')
+print()
+for num in range(10,20):
+    print(num,end='')
+print()
+for num in range (1,10,20):
+    print(num,end='')
+    '''
+    
+for num in range(5):
+    print(num,end = '')
+print(" ")
+
+for num in range (10,20):
+    print(num,end=" ")
+print(" ")
+
+for num in range (1,10,20):
+    print(num,end=" ")
+print(" ")
