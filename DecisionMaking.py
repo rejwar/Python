1f98c50ba72ef4d4c04b26c190ba9e14492b9693
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-13 11:16:18 +0600

    Create DecisionMaking.py
    Signed-off-by: Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>

---
+++ DecisionMaking.py
@@ -0,0 +1,37 @@
+'''var = 100
+if (var == 100) : print("Value of expression is 100")
+print("Good bye !")
+
+''
+
+var = 10
+if (var ==100):
+    print("value of var is equal to 100")
+else :
+    print("Value of var is not equal to 100")
+    
+    
+
+var = 100
+if (var ==100):
+    print("the number is equal to 100")
+    if var % 2 ==0:
+        print("Number is even ")
+    else:
+        print("The given number is odd")
+elif var ==0:
+    print("The number is given is 0")
+else:
+    print("The given number is unindented")
+    '''
+var = 100
+if (var ==100):
+        print("the number is 100")
+        if var %2 == 0:
+            print("Number is even")
+        else:
+            print("The given number is odd")
+elif var ==0:
+    print("The given number is zero")
+else:
+    print("The given number is negative ")
