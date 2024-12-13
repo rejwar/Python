2e98672e72faf3b790eeb5af1e1fd8e487c9adec
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-13 11:31:31 +0600

    Create Satalement.py
    Signed-off-by: Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>

---
+++ Satalement.py
@@ -0,0 +1,14 @@
+# if expression 
+# statements (s)nto be executed
+
+
+'''discount = 0
+amount = 1200
+
+if amount >1000:
+    discount = amount * 10 /100
+    
+print("amount: " , discount+ amount)
+'''
+
+
