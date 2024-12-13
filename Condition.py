4903e6d1c88dfef35c6dba3eee899c4a1c1212f8
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-13 11:47:40 +0600

    Create Condition.py
    Signed-off-by: Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>

---
+++ Condition.py
@@ -0,0 +1,60 @@
+'''
+if boolean expression :
+#code block to be executed
+#when boolean_expression is true 
+else :
+# code block to be executed
+# when booolean _expression is false 
+
+if expr == True 
+stmt1
+stmt2
+stmt3
+else :
+stmt1 
+stmt2
+stmt3
+
+
+
+age = 25 
+if age>=18:
+    print("elligibility to sex")
+else:
+    print("Not eligible to do vote ")
+    
+    '''
+    
+
+
+
+
+amount =2500
+print('amount=' , amount)
+if amount >10000:
+    discount = amount * 20 /100
+else :
+    if amount >5000:
+        discount = amount * 10 /100
+    else :
+     if amount >2500:
+            discount = amount * 5/100
+     else:
+            discount =0 
+            
+            
+print("Payble amount  = ",amount -discount)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
